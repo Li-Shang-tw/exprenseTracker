a90fc8ff8ad05d7e9a1c6d67d2af4479736f4625
Li-Shang-tw <41882910+Li-Shang-tw@users.noreply.github.com>
2019-04-17 11:33:09 +0800

    route to only search

--- routes/search.js
+++ routes/search.js
@@ -1,52 +1,22 @@
 const express = require('express')
 const router = express.Router()
-const rstModels = require('../models/resturantWeb') //導入models
+const restauranttModels = require('../models/restaurant') //導入models
 
 // router to  search
 
-router.get('/search', (req, res) => {
+router.get('/', (req, res) => {
   const keyword = req.query.keyword
-  rstModels.find((err, rsts) => {
+  restauranttModels.find((err, restaurants) => {
     if (err) return console.log(err)
-    const rstSearchResults = rsts.filter(
-      rst => rst.name.toLowerCase()
+    const rstSearchResults = restaurants.filter(
+      restaurant => restaurant.name.toLowerCase()
         .includes(keyword.toLowerCase()) ||
-        rst.category.toLowerCase()
+        restaurant.category.toLowerCase()
           .includes(keyword.toLowerCase())
     )
-    res.render('index', { rst: rstSearchResults })
+    res.render('index', { restaurant: rstSearchResults })
   })
 })
-
-//sort  list
-function sorting(route, conOb) { //輸入 route 路徑 與 conObj 排序條件物件(屬性:排序方式)
-
-  router.get(route, (req, res) => {
-    rstModels.find({})
-      .sort(conOb)
-      .exec((err, rst) => {
-        if (err) return console.error(err)
-        return res.render('index', { rst })
-      })
-
-  })
-}
-
-//name
-sorting('/sortByName', { name: 'asc' })
-
-
-//category
-sorting('/sortByCategory', { category: 'asc' })
-
-
-//LOCATION
-
-sorting('/sortBylocation', { location: 'asc' })
-
-//rating
-
-sorting('/sortByRate', { rating: 'asc' })
 
 
 
