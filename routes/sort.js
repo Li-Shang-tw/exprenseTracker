75476948b02565ec899d5f39ae3e20af14380737
Li-Shang-tw <41882910+Li-Shang-tw@users.noreply.github.com>
2019-04-17 11:28:29 +0800

    Update sort.js

--- routes/sort.js
+++ routes/sort.js
@@ -1,1 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const restauranttModels = require('../models/restaurant') //導入models
 
+
+
+
+router.get('/:sortArg', (req, res) => {
+  restauranttModels.find({})
+    .sort(req.params.sortArg)
+    .exec((err, restaurant) => {
+      if (err) return console.error(err)
+      return res.render('index', { restaurant })
+    })
+
+})
+
+
+module.exports = router
